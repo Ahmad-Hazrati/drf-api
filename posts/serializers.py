8d717a42fcacc2772d6725998dd02b41fd36399b
Ahmad Hazrati <hazrati.ahmad@gmail.com>
2023-11-03 20:30:04 +0000

    Create postlist view

--- posts/serializers.py
+++ posts/serializers.py
@@ -27,7 +27,7 @@ class PostSerializer(serializers.ModelSerializer):
         request = self.context['request']
         return request.user == obj.owner
 
-    def Meta:
+    class Meta:
         model = Post
         fields = [
             'id', 'owner', 'is_owner', 'profile_id',

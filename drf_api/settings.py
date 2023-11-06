0709a86a1518432e2fe46ba63444a248ed6e3674
Ahmad Hazrati <hazrati.ahmad@gmail.com>
2023-11-06 09:09:50 +0000

    Add Allowed_host and client_orign_dev variables for the working of API

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os 
 import dj_database_url
+import re
 
 if os.path.exists('env.py'):
     import env
@@ -37,7 +38,10 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = ['8000-ahmadhazrati-drfapi-1gslv83apfc.ws-eu105.gitpod.io', 'drf-api-blog-e8bc679bb796.herokuapp.com']
+ALLOWED_HOSTS = [os.environ.get(
+    'ALLOWED_HOST'
+    ),
+    '8000-ahmadhazrati-drfapi-1gslv83apfc.ws-eu105.gitpod.io']
 
 
 # Application definition
@@ -106,13 +110,10 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-if 'CLIENT_ORIGIN' in os.environ:
-    CORS_ALLOWED_ORIGINS = [
-        os.environ.get('CLIENT_ORIGIN')
-    ]
-else:
+if 'CLIENT_ORIGIN_DEV' in os.environ:
+    extracted_url = re.match(r'^.+-', os.environ.get('CLIENT_ORIGIN_DEV', ''), re.IGNORECASE).group(0)
     CORS_ALLOWED_ORIGIN_REGEXES = [
-        r"^https://.*\.gitpod\.io$",
+        rf"{extracted_url}(eu|us)\d+\w\.gitpod\.io$",
     ]
 
 CORS_ALLOW_CREDENTIALS = True
